fb8960e305c57d268069911cb62e0f1781843061
Emilio <emi00@gmail.com>
2022-11-30 22:16:00 +0100

    Create the package configuration, controller and the configuration of application properties

---
+++ src/main/java/it/develhope/CORS/CorsApplication.java
@@ -0,0 +1,13 @@
+package it.develhope.CORS;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class CorsApplication {
+
+}
